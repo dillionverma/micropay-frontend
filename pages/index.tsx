77a53edfc66fa947d369d75ce98ddd397f06da87
Haseab <haseab.amin@gmail.com>
2022-11-12 19:57:12 -0500

    ♿️ Add Error Handling of Content Filtering

--- pages/index.tsx
+++ pages/index.tsx
@@ -226,7 +226,16 @@ const Home: NextPage = () => {
   const largeScreen = useMediaQuery(theme.breakpoints.up("md"));
 
   const generateButtonHandler = async () => {
-    let flagged = await axios.post(`${SERVER_URL}/check-prompt`, { prompt });
+    let flagged = { data: false };
+    try {
+      flagged = await axios.post(`${SERVER_URL}/check-prompt`, { prompt });
+    } catch (e) {
+      setErrorMessage(
+        "checking for prompt flagging failed, continuing without..."
+      );
+      //implement a blocking sleep for 2 seconds
+      await new Promise((r) => setTimeout(r, 2000));
+    }
     if (!prompt) {
       setErrorMessage("Please enter a prompt");
     } else if (filter.isProfane(prompt)) {
