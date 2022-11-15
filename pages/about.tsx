65b6108fbeb51a0fe2cd4d95ae6df52627d2eea4
Haseab <59128529+haseab@users.noreply.github.com>
2022-11-15 12:48:20 -0500

    hullah3000/micro 97 add educational content to how to use (#23)
    * ♿️ Add About Section
    
    * ♿️ Add How to Use Text (WIP)
    
    * 🚸 Added Pictures and Expanded on How to Use Page
    
    * 💬 texts: Edit copy and update some styles
    
    * 🎨 style: remove grid
    
    * 🐛 fix: adjust grid
    
    Co-authored-by: Dillion Verma <hello@dillionverma.com>

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,80 @@
+import { Container, Grid } from "@mui/material";
+
 export default function About() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-      }}
-    >
-      <h1>About</h1>
-    </div>
+    <>
+      <Container
+        maxWidth="md"
+        style={{
+          marginTop: "100px",
+          textAlign: "center",
+          marginBottom: "10vh",
+        }}
+      >
+        <Grid item md={12}>
+          <Grid item md={12}>
+            <h1> About </h1>
+            <img
+              src="8b127d0ebb5d-0b9052da2e6a.jpeg"
+              alt="Dillion"
+              className="dillion"
+              width="250px"
+              style={{ marginLeft: "2%", marginRight: "2%" }}
+            />
+            <img
+              src="6ff6005b63b85066a648eff4db3ea42671caa3986b2a8e7729da8073c4a7ef04.png"
+              alt=""
+              className="haseab"
+              width="250px"
+              style={{ marginLeft: "2%", marginRight: "2%" }}
+            />
+          </Grid>
+          <Grid item md={12}>
+            <p>
+              Micropay was founded by{" "}
+              <a
+                href="https://twitter.com/dillionverma"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "#229ED9" }}
+              >
+                Dillion
+              </a>
+              {" and "}
+              <a
+                href="https://twitter.com/haseabamin"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "#229ED9" }}
+              >
+                Haseab
+              </a>
+              . It started out as a simple solution to a problem we faced once
+              we got access to OpenAI's Dalle-2 in late 2022. In order to use
+              the text to image generator, we needed to pay $15 for 115 credits
+              of use.
+            </p>
+            <p>
+              We decided to solve our problem by creating a wrapper around the
+              OpenAI API allowing us to pay-per-use instead of prepaying in
+              bulk. Payments were accepted over the lightning network and the
+              product is completely anonymous allowing anyone to use it without
+              having to sign up. It's like a vending machine, but for software.
+            </p>
+            <p>
+              This solution is unique since traditional credit card providers
+              can't handle microtransactions due to their high fees. The
+              Lightning Network however, which is a technology built on Bitcoin,
+              allows for instant and low-fee microtransactions at scale.
+            </p>
+            {/* <p>
+          We envision making an array of tools for small businesses, making it
+          easier for them to get off the ground by making it anonymous, low
+          friction and pay per use.
+        </p> */}
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
--- pages/how-to-use.tsx
+++ pages/how-to-use.tsx
@@ -1,14 +1,105 @@
+import { Container, Grid, Tooltip } from "@mui/material";
+
 export default function HowTo() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-      }}
+    <Container
+      maxWidth="md"
+      style={{ marginTop: "100px", textAlign: "center", marginBottom: "10vh" }}
     >
       <h1>How to Use</h1>
-    </div>
+      <div className="how-to-use-intro">
+        <p style={{ fontSize: "20px" }}>
+          This product is a “pay as you go” version of Open AI’s Dalle-2. This
+          means you <strong>don't have</strong> to spend $15 for 115 generations
+          at a time, and instead purchase one at a time if you would like. Since
+          these micropayments are done on the lightning network, this means that
+          you must have Cashapp / Strike or a lightning wallet to pay the
+          lightning invoices that are served
+        </p>
+        <br></br>
+      </div>
+
+      <h2>Demos Below </h2>
+
+      <Grid container direction="row">
+        <Grid className="cashapp-demo" item xs={12} md={4}>
+          <img
+            src="438r9893urkcks7339cj38.png"
+            alt="cashapp"
+            className="cashapp"
+            height="100px"
+          />
+          <Tooltip title="Not Available Yet" placement="top">
+            <h2 style={{ marginTop: "-1.5vh" }}>Demo</h2>
+          </Tooltip>
+        </Grid>
+
+        <Grid className="strike-demo" item xs={12} md={4}>
+          <img
+            src="3488f6cc678b98cb97a9a7a502ce38adab2d9a7ff88c25900f2c79b51ec8df69.png"
+            alt="strike-logo"
+            className="strike-png"
+            height="75"
+            style={{ marginTop: "1vh" }}
+          />
+
+          <Tooltip title="Not Availabe Yet" placement="top">
+            <h2 style={{ marginTop: "0vh" }}>Demo</h2>
+          </Tooltip>
+        </Grid>
+
+        <Grid className="lightning-wallet-demo" item xs={12} md={4}>
+          <img
+            src="8395dcb24914bb7f5ee0e94f08d2ddcb0b3b1339ffad4dd50b7c3d3adc65f635.png"
+            alt="ln-icon"
+            height="30px"
+            className="ln-icon"
+            style={{ marginTop: "3vh" }}
+          />
+          <h2 style={{ display: "inline" }}>LN Wallet</h2>
+          <h2>
+            <a
+              href="https://twitter.com/dillionverma/status/1567508869944938503"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#229ED9" }}
+            >
+              Demo
+            </a>
+          </h2>
+        </Grid>
+      </Grid>
+      <Grid md={12}>
+        <br></br>
+        <h2>See Below for more Info</h2>
+        <p style={{ fontSize: "20px", marginTop: "-5px" }}>
+          How to use your
+          <a
+            href="https://youtu.be/Cfc3JGb3a4g?t=492"
+            style={{ color: "#229ED9" }}
+          >
+            {" "}
+            Bitcoin on the Lightning Network
+          </a>
+        </p>
+        <p style={{ fontSize: "20px", marginTop: "-5px" }}>
+          How to Convert
+          <a href="https://youtu.be/LxkrUvrfNAI" style={{ color: "#229ED9" }}>
+            {" "}
+            Altcoins to Bitcoin{" "}
+          </a>
+        </p>
+        <p style={{ fontSize: "20px", marginTop: "-5px" }}>
+          How to
+          <a
+            href="https://www.youtube.com/watch?v=Yy_R2C0tpqU"
+            style={{ color: "#229ED9" }}
+          >
+            {" "}
+            Buy Bitcoin{" "}
+          </a>
+        </p>
+      </Grid>
+    </Container>
   );
 }
