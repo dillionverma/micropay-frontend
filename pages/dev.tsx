557f8568dafaabc7c77d5e9e02e0ce521a5071bc
Dillion Verma <hello@dillionverma.com>
2022-12-06 14:54:28 -0500

    🐛 fix: Add landing page metatags

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -1,8 +1,56 @@
+import Head from "next/head";
 import { ReactElement } from "react";
 
 const Dev = () => {
   return (
     <>
+      <Head>
+        <title>micropay</title>
+        <meta name="description" content="Micropay" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Lightning Payments API for developers | micropay</title>
+        <meta
+          name="title"
+          content="Lightning Payments API for developers | micropay"
+        />
+
+        <meta
+          name="description"
+          content="Instantly integrate the Lightning Network into your app without managing a node ⚡."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://micropay.ai/" />
+        <meta
+          property="og:title"
+          content="Lightning Payments API for developers | micropay"
+        />
+        <meta
+          property="og:description"
+          content="Instantly integrate the Lightning Network into your app without managing a node ⚡."
+        />
+        <meta
+          property="og:image"
+          content="https://micropay.ai/micropay-dev-preview.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://micropay.ai/" />
+        <meta
+          property="twitter:title"
+          content="Lightning Payments API for developers | micropay"
+        />
+        <meta
+          property="twitter:description"
+          content="Instantly integrate the Lightning Network into your app without managing a node ⚡."
+        />
+        <meta
+          property="twitter:image"
+          content="https://micropay.ai/micropay-dev-preview.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <iframe
         id="micropay-iframe"
         src="https://micropay-dev.framer.website/"
